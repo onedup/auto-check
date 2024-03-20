6f12eae9af4f8a8fd836c3ed9dada2f1473abb5c
onedup <nikulichevmikhail@gmail.com>
2024-03-20 12:52:35 +0400

    header and hero adaptation

--- src/js/components/video-slider.js
+++ src/js/components/video-slider.js
@@ -1,13 +1,13 @@
 import Swiper from "swiper/bundle";
 
 const swiper = new Swiper(".video-swiper", {
+  loop: true,
   slidesPerView: 4,
   spaceBetween: 60,
-  
+
   // Navigation arrows
   navigation: {
     nextEl: ".video-swiper-button-next",
     prevEl: ".video-swiper-button-prev",
   },
-
 });
